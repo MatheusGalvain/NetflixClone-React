193cfffc1691f239583997eda183caa936767900
Matheus <matheusgalvain@gmail.com>
2022-05-05 03:07:34 -0300

    Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie'; './components/FeaturedMovie'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+export default () => {
+
+  const[movieList, setMovieList] = useState([]);
+  const[featuredData, setFeaturedData] = useState(null);
+
+ 
+  useEffect(()=>{
+    const loadAll = async() => {
+      //Pegando toda a lista
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      //Pegando o Featured
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.lenght - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      
+      console.log(chosen);
+    }
+
+    loadAll();
+  }, []);
+
+
+  return(
+    <div className="page">
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />
+      }
+
+      <section className="lists">
+        {movieList.map((item,key) => (
+         <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+      </section>
     </div>
-  );
+  )
 }
 
-export default App;
+
--- src/Tmdb.js
+++ src/Tmdb.js
@@ -1,2 +1,66 @@
 const API_KEY = 'b1e108c55a82f49619193af036e7d04d';
-const API_BASE = 'https://api.themoviedb.org/3/';
+const API_BASE = 'https://api.themoviedb.org/3';
+
+// - Originais 
+// - Recomendados
+// - Em alta
+// - Ação
+// - Comedia
+// - Terror 
+// - Ficcao
+// - Documentarios
+
+// Requisita o json e retorna resultado
+const basicFetch = async (endpoint) => {
+    const req = await fetch(`${API_BASE}${endpoint}`);
+    const json = await req.json();
+    return json;
+}
+
+export default {
+    getHomeList: async() => {
+        return [
+            {
+                slug:'originals',
+                title: 'Originais do Netflix',
+                items: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug:'trending',
+                title: 'Recomendados para Você',
+                items: await basicFetch(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug:'toprated',
+                title: 'Em alta',
+                items: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug:'action',
+                title: 'Ação',
+                items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug:'Mystery',
+                title: 'Mistério',
+                items: await basicFetch(`/discover/movie?with_genres=9648&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug:'horror',
+                title: 'Terror',
+                items: await basicFetch(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug:'fantasy',
+                title: 'Fantasia',
+                items: await basicFetch(`/discover/movie?with_genres=14&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug:'documentary',
+                title: 'Documentário',
+                items: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
+            },
+        ]
+    }
+    
+}
